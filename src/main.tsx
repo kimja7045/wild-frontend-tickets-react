9b48b0f050e58469813fb7a9b51718eca158daab
김재훈 <temp7045@gmail.com>
2025-04-16 22:41:21 +0900

    feat: App 컴포넌트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import { App } from '../App';
+
 const container = document.getElementById('root');
 if (container) {
-  console.log(container);
+  const root = createRoot(container);
+  root.render(<App />);
 }
